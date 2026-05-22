d97cf1a4a85f6ac4a7512cc5fd9106874c571f14
GitHub Action <action@github.com>
2026-05-22 02:38:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/cpardi/intellijmarkdownreviewplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/cpardi/intellijmarkdownreviewplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.cpardi.intellijmarkdownreviewplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.cpardi.intellijmarkdownreviewplugin.MyBundle
+import com.github.cpardi.intellijmarkdownreviewplugin.services.MyProjectService
 import javax.swing.JButton
 
 
